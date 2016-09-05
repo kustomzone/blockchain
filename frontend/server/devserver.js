fa6952033ea12bc98e60d920c896f24d294e583c
Truls Clark <truls.clark@gmail.com>
2016-09-05 19:03:26 +0200

    tweaking av devscript

--- frontend/server/devserver.js
+++ frontend/server/devserver.js
@@ -23,7 +23,7 @@ compiler.plugin('done', function () {
 });
 
 new WebpackDevServer(compiler, {
-	publicPath: config.output.publicPath,
+	// publicPath: config.output.publicPath,
 	hot: true,
 	inline: true,
 	historyApiFallback: true,
@@ -33,7 +33,7 @@ new WebpackDevServer(compiler, {
 	stats: {
 		colors: true
 	}
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, '0.0.0.0', function (err, result) {
 	if (err) console.log(err);
 	console.log('Listening at localhost:3000');
 });
