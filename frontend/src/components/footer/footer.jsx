533042f0641a64cc61c24cb056662882a90e1df0
unknown <tclark@LNOR010395.corp.capgemini.com>
2016-09-08 15:28:36 +0200

    footer...

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
     return(
       <footer>
-        This is the footer
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </footer>
     )
   }
